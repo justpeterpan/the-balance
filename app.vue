5e09957d53afd0fcd3af78ffc53273eafab3ff6d
justpeterpan <peter.busch@posteo.net>
2024-06-02 21:48:17 +0200

    chore: install cheerio

--- app.vue
+++ app.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="flex flex-row gap-2 m-10">
-    <UInput type="text" v-model="urlToBookmark" />
-    <UButton @click="submitUrl">Submit</UButton>
+  <div class="grid m-10 gap-4 w-96">
+    <div class="flex flex-row justify-between">
+      <UInput
+        type="text"
+        v-model="urlToBookmark"
+        size="xl"
+        class="w-72"
+        placeholder="url to bookmark"
+      />
+      <UButton @click="submitUrl" size="xl">Submit</UButton>
+    </div>
+    <UCard v-if="desc && image">
+      <template #header>
+        <div>{{ title }}</div>
+      </template>
+      <img :src="image" alt="" />
+      <template #footer>
+        {{ desc }}
+      </template>
+    </UCard>
   </div>
 </template>
 
@@ -11,10 +28,18 @@ const urlToBookmark = defineModel('urlToBookmark', {
   default: '',
 })
 
+const desc = ref()
+const title = ref()
+const image = ref()
+
 async function submitUrl() {
-  await $fetch('/c', {
+  const { i, d, t } = await $fetch('/c', {
     method: 'POST',
     body: JSON.stringify({ url: urlToBookmark.value }),
   })
+
+  desc.value = d
+  title.value = t
+  image.value = i
 }
 </script>
--- server/routes/c.ts
+++ server/routes/c.ts
@@ -1,5 +1,17 @@
+import * as cheerio from 'cheerio'
+
 export default defineEventHandler(async (event) => {
   const { url } = await readBody(event)
-  console.log(url)
-  return url
+  const site = await $fetch<string>(url)
+  return getMetaInfo(site)
 })
+
+function getMetaInfo(site: string) {
+  const $ = cheerio.load(site)
+
+  const i = $('meta[property="og:image"]').attr('content')
+  const d = $('meta[property="og:description"]').attr('content')
+  const t = $('meta[property="og:title"]').attr('content')
+
+  return { i, d, t }
+}
